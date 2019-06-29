c70be3793c6b39d3fa08a3b86c1a70b7fdae5143
momo <mmko5110@gmail.com>
2019-06-29 19:02:04 +0900

    first-commit

---
+++ app/src/main/java/com/example/low_pressure_check/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.low_pressure_check
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
