17cf90b4126385dd1934f5c9eab660cd238bc98c
momo <mmko5110@gmail.com>
2019-06-30 18:43:11 +0900

    ApiClientの作成

---
+++ app/src/main/java/com/example/low_pressure_check/model/ApiClient.kt
@@ -0,0 +1,40 @@
+package com.example.low_pressure_check.model
+
+import com.example.low_pressure_check.BuildConfig
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiClient {
+    private const val BASE_URL = "https://api.darksky.net/forecast/"
+
+    val moshi: Moshi = Moshi.Builder()
+        .build()
+
+    var retrofit: Retrofit
+
+    init {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                if (BuildConfig.DEBUG) {
+                    level = HttpLoggingInterceptor.Level.BODY
+                } else {
+                    // TODO: releaseビルドではログを非表示にする
+                }
+            })
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+//            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .baseUrl(BASE_URL)
+            .build()
+    }
+}
