44b9ebd383daba6eb158bbbab32625889b697d0d
momo <mmko5110@gmail.com>
2019-07-07 19:08:40 +0900

    viewModelを追加

--- app/src/main/java/com/example/low_pressure_check/model/Repository.kt
+++ app/src/main/java/com/example/low_pressure_check/model/Repository.kt
@@ -3,7 +3,7 @@ package com.example.low_pressure_check.model
 import retrofit2.Response
 import retrofit2.Retrofit
 
-class Repositry(private val retrofit: Retrofit) {
+class Repository(private val retrofit: Retrofit) {
     private val service by lazy { retrofit.create(Service::class.java) }
 
     suspend fun getForecast(): Response<ForecastEntity> {
