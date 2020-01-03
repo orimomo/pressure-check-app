c4b233256089488f0a759bb7abe1dcdc813e3473
momo <mmko5110@gmail.com>
2020-01-03 18:58:46 +0900

    不要なコード削除

--- app/src/main/java/com/example/low_pressure_check/view/MainActivity.kt
+++ app/src/main/java/com/example/low_pressure_check/view/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         // ViewModelにライフサイクルを連動させるために必要
         lifecycle.addObserver(viewModel)
